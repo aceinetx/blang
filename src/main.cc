6560363a7ab467278b1dfd538e29ad63148ec802
aceinetx <mouselin82@gmail.com>
2026-04-23 14:50:23 +0000

    damn this shit complex

--- src/main.cc
+++ src/main.cc
@@ -8,6 +8,21 @@ int main(int argc, char **argv) {
 main(argc, argv) {
 	extrn puts;
 	goto a;
+	while(argc > 0){
+		if(argc == 1){
+			puts("1");
+		} else if(argc == 2){
+			puts("2");
+		} else if(argc == 3){
+			puts("2");
+		} else if(argc == 4){
+			puts("4");
+		} else {
+			puts("other");
+		}
+		a:
+		argc = argc - 1;
+	}
 	return(0);
 }
 )");
