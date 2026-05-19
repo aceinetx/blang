c172e10c3ec1ed3d87b5c1611e9054b43e642cd1
aceinetx <mouselin82@gmail.com>
2026-05-19 18:23:47 +0300

    array example

--- examples/xmake.lua
+++ examples/xmake.lua
@@ -2,10 +2,9 @@ add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"}) -- generate compile commands
 
 includes("../rules/blang.lua")
+add_rules("c", "c++", "blang")
 
 target("example-curses")
-	add_rules("c", "c++", "blang")
-	add_rules("blang")
 	set_kind("binary")
 
 	add_files("curses.b")
@@ -13,37 +12,38 @@ target("example-curses")
 target_end()
 
 target("example-duffs_device")
-	add_rules("blang")
 	set_kind("binary")
 
 	add_files("duffs_device.b")
 target_end()
 
 target("example-hello")
-	add_rules("blang")
 	set_kind("binary")
 
 	add_files("hello.b")
 target_end()
 
 target("example-mandelbrot")
-	add_rules("blang")
 	set_kind("binary")
 
 	add_files("mandelbrot.b")
 target_end()
 
 target("example-turing")
-	add_rules("blang")
 	set_kind("binary")
 
 	add_files("turing.b")
 target_end()
 
 target("example-c_interop")
-	add_rules("c", "c++", "blang")
 	set_kind("binary")
 
 	add_files("c_interop/*.c")
 	add_files("c_interop/*.b")
+target_end()
+
+target("example-array")
+	set_kind("binary")
+
+	add_files("array.b")
 target_end()
