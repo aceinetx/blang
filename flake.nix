6b6d3325bdb818fc1de79c409d944ab14a164f4b
aceinetx <mouselin82@gmail.com>
2026-06-26 16:23:07 +0300

    package flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,49 @@
 {
-  description = "blang";
+  description = "blang flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "blang";
+          version = "1.0.0";
+          src = self;
+
+          buildPhase = ''
+            xmake b
+            xmake i -o .
+          '';
+
+          buildInputs = with pkgs; [
+            gcc
+            xmake
+            pkg-config
+            llvm
+            fmt
+            bison
+            m4
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp bin/blang $out/bin/
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             gcc
